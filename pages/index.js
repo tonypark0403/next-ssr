2ebf1a60858290157f754283545790e4bcb0f114
Tony Park <tonypark0403@gmail.com>
2020-03-29 20:50:52 -0400

    initiailized project

---
+++ pages/index.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Home extends Component {
+    render() {
+        return (
+            <>
+                <h1>Welcome to my page</h1>
+            </>
+        )
+    }
+}
+
+export default Home;
