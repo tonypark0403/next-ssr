02a1179695baecd615e0975777f8222a06bec626
Tony Park <tonypark0403@gmail.com>
2020-03-29 20:58:53 -0400

    used getInitProps to avoid unnecessary rendering

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,37 @@
 import React, { Component } from 'react';
+import axios from 'axios';
+
 import MainLayout from '../components/layouts/mainLayout';
 
 class Home extends Component {
+    static async getInitialProps() {
+        //console.log("getInitialProps")
+        let userData;
+        try {
+            const response = await axios.get(`https://jsonplaceholder.typicode.com/users/1`)
+            userData = response.data;
+            // console.log(userData);
+        } catch {
+            console.log('server error');
+        }
+        return {
+            user: {
+                name: 'Tony',
+                lastname: 'Park'
+            },
+            userData
+        };
+    }
+    constructor(props) {
+        super(props);
+        // console.log("constructor");
+        this.state = {
+            user: this.props.user,
+            userData: this.props.userData
+        }
+    }
     render() {
+        console.log(this.state);
         return (
             <>
                 <MainLayout>
